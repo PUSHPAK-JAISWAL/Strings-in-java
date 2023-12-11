6842599a0b3ef813d49b663ab35b8cea294a3457
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2023-12-11 16:29:03 +0530

    Add files via upload

---
+++ S1.java
@@ -0,0 +1,9 @@
+//this is the basic program of string for example
+public class S1 
+{ 
+    public static void main (String args[])
+    {
+        String brand=" pushpak_jaiswal";
+        System.out.println(brand);
+    }
+}
---
+++ S2.java
@@ -0,0 +1,11 @@
+// in this program we are gonna use the other ways to input the string that are string builder and string buffer
+public class S2 
+{
+    public static void main(String args[])
+    {
+        StringBuilder brand= new StringBuilder("pushpak_jaiswal ");// this is the example for the string builder
+        System.out.println(brand);
+        brand.append(" is in aurangabad");
+        System.out.println(brand);
+    }
+}
